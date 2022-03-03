43bd6767437110cd12a29185a2dcda7cea209e64
HuguesBriqueler <hugues.briqueler@gmail.com>
2022-03-04 00:55:45 +0100

    First test passed - 2nd test is red

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Button has correct initial text & color", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // find element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("Button turns blue when clicked", () => {
+  render(<App />);
+  const colorButton = screen.getByRole("button", { name: "Change to blue" });
+  // click the button
+  fireEvent.click(colorButton);
+  // expect background change to blue and text change to 'Change to red'
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+  expect(colorButton.textContent).toBe("Change to red");
 });
