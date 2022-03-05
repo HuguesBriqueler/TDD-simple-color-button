09b34904f7010bb3c8d73045f8762868df07b595
HuguesBriqueler <hugues.briqueler@gmail.com>
2022-03-05 05:34:45 +0100

    made the component to be controlled + test still green

--- src/App.js
+++ src/App.js
@@ -2,27 +2,34 @@ import { useState } from "react";
 
 function App() {
   const [colorButton, setColorButton] = useState("red");
-  // const [isBoxChecked, setIsBoxChecked] = useState(false);
+  const [isBoxChecked, setIsBoxChecked] = useState(false);
   const nextColorValue = colorButton === "red" ? "blue" : "red";
 
   const handleCheckbox = (event) => {
-    // setIsBoxChecked(event.value);
-    const buttonElement = document.querySelector("button");
-    if (event.target.checked) {
-      buttonElement.setAttribute("disabled", "");
-    } else {
-      buttonElement.removeAttribute("disabled");
-    }
+    setIsBoxChecked(event.target.checked);
+    // const buttonElement = document.querySelector("button");
+    // if (event.target.checked) {
+    //   buttonElement.setAttribute("disabled", "");
+    // } else {
+    //   buttonElement.removeAttribute("disabled");
+    // }
   };
   return (
     <div>
       <button
         style={{ backgroundColor: colorButton }}
         onClick={() => setColorButton(nextColorValue)}
+        disabled={isBoxChecked}
       >
         Change to {nextColorValue}
       </button>
-      <input type={"checkbox"} onChange={handleCheckbox} />
+      <input
+        type={"checkbox"}
+        id="enable-button-checkbox"
+        defaultChecked={isBoxChecked}
+        aria-checked={isBoxChecked} // for accessibility (e.g. screen reader)
+        onChange={handleCheckbox}
+      />
     </div>
   );
 }
